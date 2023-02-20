3902c61ef304f68a529c6f52ecbaa4b1a5fff509
Delmar Caca <delmar.caca1991@gmail.com>
2023-02-20 23:26:29 +0800

    Updates

---
+++ precache-manifest.9c1fb389f16f46e5fb77a9b7e4bf92f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "04d636a26f87c8ab2089",
+    "url": "/mp2-test/static/css/main.c7a3758a.chunk.css"
+  },
+  {
+    "revision": "04d636a26f87c8ab2089",
+    "url": "/mp2-test/static/js/main.04d636a2.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/mp2-test/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/mp2-test/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "5d6bf0ec1f2af914f8c4",
+    "url": "/mp2-test/static/js/runtime~main.5d6bf0ec.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/mp2-test/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "235c7a850e2bba724271c4513f8022e6",
+    "url": "/mp2-test/index.html"
+  }
+];
